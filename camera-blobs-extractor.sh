3a9857c8e3fc1431e5eedc16c22fc1946d3502ab
YumeMichi <do4suki@gmail.com>
2018-03-21 19:37:07 +0800

    Update camera-blobs-extractor.sh
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- camera-blobs-extractor.sh
+++ camera-blobs-extractor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Old camera HAL blobs
-libs=" \
+# Sultan camera HAL blobs
+SULTAN_LIBS=" \
 vendor/lib/libjpegdhw.so
 vendor/lib/libjpegehw.so
 vendor/lib/libmmcamera2_c2d_module.so
@@ -58,35 +58,17 @@ vendor/lib/libchromatix_s5k3m2_preview.so
 vendor/lib/libchromatix_s5k3m2_snapshot.so
 vendor/lib/libchromatix_s5k3m2_zsl.so"
 
-bins="vendor/bin/mm-qcamera-daemon"
+SULTAN_FWS=" \
+vendor/firmware/a330_pfp.fw
+vendor/firmware/a330_pm4.fw
+vendor/firmware/cpp_firmware_v1_1_1.fw
+vendor/firmware/cpp_firmware_v1_1_6.fw
+vendor/firmware/cpp_firmware_v1_2_0.fw"
 
-fmws=" \
-etc/firmware/cpp_firmware_v1_1_1.fw
-etc/firmware/cpp_firmware_v1_1_6.fw
-etc/firmware/cpp_firmware_v1_2_0.fw"
-
-root="/root/proprietary_vendor_oneplus/"
-url="https://raw.githubusercontent.com/TheMuppets/proprietary_vendor_oneplus/lineage-15.0/"
-
-binarr=(${bins})
-libarr=(${libs})
-fmwarr=(${fmws})
-retarr=(${binarr[@]}" "${libarr[@]}" "${fmwarr[@]})
-
-# Remove old camera HAL blobs
-for i in ${retarr[@]}
-do
-	rm -rf ${root}"onyx/proprietary/"${i}
-	str=${i//\//\\\/}
-	# echo $str
-	sed "/${str}/d" -i ${root}"onyx/onyx-vendor.mk"
-done
-
-
-# New camera HAL blobs
-libs="\
-lib/libFNVfbEngineLib.so
-lib/libopcamerahw_interface.so
+# OOS3 camera HAL blobs
+OOS3_LIBS="\
+vendor/lib/libFNVfbEngineLib.so
+vendor/lib/libopcamerahw_interface.so
 vendor/lib/libjpegdhw.so
 vendor/lib/libjpegehw.so
 vendor/lib/libmmcamera2_c2d_module.so
@@ -130,6 +112,8 @@ vendor/lib/libmmipl.so
 vendor/lib/libmmjpeg.so
 vendor/lib/libmmqjpeg_codec.so
 vendor/lib/liboemcamera.so
+vendor/lib/libqomx_jpegdec.so
+vendor/lib/libqomx_jpegenc.so
 vendor/lib/libactuator_dw9714.so
 vendor/lib/libactuator_dw9714_camcorder.so
 vendor/lib/libactuator_dw9714_camera.so
@@ -155,35 +139,85 @@ vendor/lib/libchromatix_s5k3m2_preview_panorama.so
 vendor/lib/libchromatix_s5k3m2_snapshot.so
 vendor/lib/libchromatix_s5k3m2_snapshot_panorama.so
 vendor/lib/libchromatix_s5k3m2_video_hd.so
-vendor/lib/libchromatix_s5k3m2_zsl.so
-vendor/lib/libqomx_jpegdec.so
-vendor/lib/libqomx_jpegenc.so"
+vendor/lib/libchromatix_s5k3m2_zsl.so"
 
-bins="bin/mm-qcamera-daemon"
+OOS3_FWS=" \
+vendor/firmware/a330_pfp.fw
+vendor/firmware/a330_pm4.fw
+vendor/firmware/cpp_firmware_v1_1_1.fw
+vendor/firmware/cpp_firmware_v1_1_6.fw
+vendor/firmware/cpp_firmware_v1_2_0.fw"
 
-fmws=" \
-etc/firmware/cpp_firmware_v1_1_1.fw
-etc/firmware/cpp_firmware_v1_1_6.fw
-etc/firmware/cpp_firmware_v1_2_0.fw"
+ROOT_DIR="/home/ikke/Work/proprietary_vendor_oneplus_tmp/"
+OOS3_URL="https://raw.githubusercontent.com/YumeMichi/proprietary_vendor_oneplus/o-mr1/"
+SULTAN_URL="https://raw.githubusercontent.com/YumeMichi/proprietary_vendor_oneplus/lineage-15.1-sultan/"
 
-binarr=(${bins})
-libarr=(${libs})
-fmwarr=(${fmws})
-retarr=(${binarr[@]}" "${libarr[@]}" "${fmwarr[@]})
+SULTAN_LIBS=(${SULTAN_LIBS})
+SULTAN_FWS=(${SULTAN_FWS})
+SULTAN_BLOBS=${SULTAN_LIBS[@]}" "${SULTAN_FWS[@]}
 
-# echo ${retarr[@]}
+OOS3_LIBS=(${OOS3_LIBS})
+OOS3_FWS=(${OOS3_FWS})
+OOS3_BLOBS=${OOS3_LIBS[@]}" "${OOS3_FWS[@]}
 
-# Remove old camera HAL blobs
-for i in ${retarr[@]}
-do
-	cd $root
-	if [[ $i =~ "bin" ]]
-	then
-		if [ ! -d "onyx/proprietary/bin" ]
-		then
-			/usr/bin/mkdir "onyx/proprietary/bin"
+SyncCameraHAL() {
+	TEMP1=$1
+	TEMP2=$2
+	MODE=$3
+	FLAG=1
+
+	for b in ${TEMP1[@]}
+	do
+		BLOB=${b//\//\\\/}
+		echo -e "Deleting...\t"${b}
+
+		FILE=${ROOT_DIR}"onyx/proprietary/"${b}
+		if [ -f "$FILE" ]; then
+			rm -rf $FILE
+			sed "/${BLOB}/d" -i ${ROOT_DIR}"onyx/onyx-vendor.mk"
+		else
+			echo "Error: $FILE not found! Cleaning up vendor tree..."
+			cd $ROOT_DIR
+			git reset --hard > /dev/null && git clean -f -d
+			FLAG=0
+			break
 		fi
+	done
+
+	if [[ "$FLAG" == "0" ]]; then
+		exit
 	fi
-	echo ${url}"onyx/proprietary/"${i}
-	wget -qO "onyx/proprietary/"$i ${url}"onyx/proprietary/"${i}
-done
+
+	for b in ${TEMP2[@]}
+	do
+		BLOB=${b//\//\\\/}
+		echo -e "Processing...\t"${b}
+		if [[ "$MODE" == "1" ]]; then
+			URL=${SULTAN_URL}
+		else
+			URL=${OOS3_URL}
+		fi
+		echo -e "Downloading...\t"${URL}"onyx/proprietary/"${b}
+		FILE=${ROOT_DIR}"onyx/proprietary/"${b}
+		wget -qO $FILE ${URL}"onyx/proprietary/"${b}
+		if [[ ! -f "$FILE" ]]; then
+			echo -e "Error: Download $FILE failed! Cleaning up vendor tree..."
+			cd $ROOT_DIR
+			git reset --hard > /dev/null && git clean -f -d
+			break
+		fi
+		# TODO
+		# Insert new lines into onyx-vendor.mk automatically.
+	done
+}
+
+# 1: Replace OOS3 camera HAL with Sultan camera HAL
+# 2: Replace Sultan camera HAL with OOS3 camera HAL
+MODE=$1
+if [[ "$MODE" == "1" ]]; then
+	SyncCameraHAL "$OOS3_BLOBS" "$SULTAN_BLOBS" "$MODE"
+elif [[ "$MODE" == "2" ]]; then
+	SyncCameraHAL "$SULTAN_BLOBS" "$OOS3_BLOBS" "$MODE"
+else
+	echo "Invalid parameter!"
+fi
